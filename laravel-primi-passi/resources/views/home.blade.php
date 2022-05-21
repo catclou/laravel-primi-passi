9ad61c341ef737c0602a73e75b332883a548f755
catclou <caterina.moccia@gmail.com>
2022-05-21 18:53:53 +0200

    dati dinamicizzati

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,3 +1,9 @@
+@php 
+    $dato1 = 'Questo è il primo dato dinamicizzato';
+    $dato2 = 'Questo è il secondo dato dinamicizzato';
+    $dato3 = 'Questo è il terzo dato dinamicizzato';
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,5 +14,8 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <h3>{{$dato1}}</h3>
+    <h3>{{$dato2}}</h3>
+    <h3>{{$dato3}}</h3>
 </body>
 </html>
